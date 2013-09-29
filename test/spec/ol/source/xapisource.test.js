5888380cdf7e8355abc058aece9c9a8646291d8f
Jachym Cepicky <jachym.cepicky@gmail.com>
2013-09-30 01:17:48 +0200

    fixing api changes

--- test/spec/ol/source/xapisource.test.js
+++ test/spec/ol/source/xapisource.test.js
@@ -4,71 +4,75 @@ goog.require('ol.source.XAPI');
 
 describe('ol.source.XAPI', function() {
 
-  var extent = [14.39811708927129, 50.07493521431255,
-    14.450817131995263, 50.086633811917075];
+      var extent = [14.39811708927129, 50.07493521431255,
+        14.450817131995263, 50.086633811917075];
 
-  describe('ol.source.XAPI', function() {
-    it('creates an instance', function() {
-      var source = new ol.source.XAPI({});
-      expect(source).to.be.a(ol.source.XAPI);
-      expect(source).to.be.a(ol.source.Vector);
-    });
-  });
-
-  describe('#getFeaturesUrl', function() {
-    it('prepares xapi url based on given extent', function() {
-      var source = new ol.source.XAPI({});
-      var url = source.getFeaturesUrl(extent);
-      expect(url).to.equal(
-          'http://xapi.openstreetmap.org/api/0.6/*[bbox=' +
-          extent.join(',') + ']');
-    });
-
-    it('prepares url based on different url then defeault', function() {
-      var source = new ol.source.XAPI({
-        url: 'http://api.openstreetmap.fr/xapi?',
-        type: 'node'});
-      var url = source.getFeaturesUrl(extent);
-      expect(url).to.equal(
-          'http://api.openstreetmap.fr/xapi?node[bbox=' +
-          extent.join(',') + ']');
-    });
-
-    it('adds different parameters to url', function() {
-      var source = new ol.source.XAPI({
-        url: 'http://api.openstreetmap.fr/xapi?'
+      describe('ol.source.XAPI', function() {
+        it('creates an instance', function() {
+          var source = new ol.source.XAPI({});
+          expect(source).to.be.a(ol.source.XAPI);
+          expect(source).to.be.a(ol.source.Vector);
+        });
       });
-      var url = source.getFeaturesUrl(extent, {
-        man_made: 'surveillance'
+
+      describe('#getFeaturesUrl', function() {
+        it('prepares xapi url based on given extent', function() {
+          var source = new ol.source.XAPI({});
+          var url = source.getFeaturesUrl(extent);
+          expect(url).to.equal(
+              'http://xapi.openstreetmap.org/api/0.6/*[bbox=' +
+              extent.join(',') + ']');
+        });
+
+        it('prepares url based on different url then defeault', function() {
+          var source = new ol.source.XAPI({
+            url: 'http://api.openstreetmap.fr/xapi?',
+            type: 'node'});
+          var url = source.getFeaturesUrl(extent);
+          expect(url).to.equal(
+              'http://api.openstreetmap.fr/xapi?node[bbox=' +
+              extent.join(',') + ']');
+        });
+
+        it('adds different parameters to url', function() {
+          var source = new ol.source.XAPI({
+            url: 'http://api.openstreetmap.fr/xapi?'
+          });
+          var url = source.getFeaturesUrl(extent, {
+            man_made: 'surveillance'
+          });
+          expect(url).to.equal(
+              'http://api.openstreetmap.fr/xapi?*[bbox=' +
+              extent.join(',') + '][man_made=surveillance]');
+        });
       });
-      expect(url).to.equal(
-          'http://api.openstreetmap.fr/xapi?*[bbox=' +
-          extent.join(',') + '][man_made=surveillance]');
-    });
-  });
 
 
-  /* can't test remote resvers
-     * works by hand
-    describe('#prepareFeatures', function() {
-        it('loads and parses data from url', function() {
+        /* can't test remote resvers */
+        describe('#prepareFeatures', function() {
+            it('loads and parses data from url', function() {
 
-            var source = new ol.source.XAPI({
-                url: 'http://api.openstreetmap.fr/xapi/?',
-                type: 'node'
-            });
+                var source = new ol.source.XAPI({
+                    url: 'spec/ol/parser/osm/node.xml?*',
+                    type: 'node'
+                });
 
-            var layer = new ol.layer.Vector({
-                source: source
-            });
+                var layer = new ol.layer.Vector({
+                    source: source
+                });
 
-            source.prepareFeatures(layer, extent, undefined,
-                function() {
-                    expect(source.loadState_).to.be(ol.source.VectorLoadState.LOADED);
+                source.prepareFeatures(layer, extent, ol.proj.get('EPSG:4326'),
+                    function() {
+                    expect(source.loadState_).to.be(
+                        ol.source.VectorLoadState.LOADED);
                     expect(goog.object.getCount(
-                        layer.featureCache_.getFeaturesObject())).to.be(179);
+                        layer.featureCache_.getFeaturesObject())).to.be(1);
+                    var features = layer.featureCache_.getFeaturesObject();
+                    var attrs = features[2].getAttributes();
+                    expect(attrs.version).to.be('2');
+                    expect(attrs.man_made).to.be('surveillance');
+                    expect(attrs.user).to.be('vaja');
                 });
             });
         });
-    */
 });
